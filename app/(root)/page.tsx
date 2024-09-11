9b327535db0dccf5c8a404fca0e3b9fa0f2ad40c
Saman Abbasi <samanabbasi.ir@gmail.com>
2024-09-11 16:30:49 +0330

    add TotalBalanceBox

---
+++ app/(root)/page.tsx
@@ -0,0 +1,23 @@
+import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+
+function Home() {
+	const LoggedIn = { firstName: 'Adrian' };
+	return (
+		<section className='home'>
+			<div className='home-content'>
+				<header className='home-header'>
+					<HeaderBox
+						type='greeting'
+						title='Welcome'
+						user={LoggedIn?.firstName || 'Guest'}
+						subtext='Access and manage your account and transactions efficiently.'
+					/>
+					<TotalBalanceBox accounts={[]} totalBank={1} totalCurrentBalance={1250.35} />
+				</header>
+			</div>
+		</section>
+	);
+}
+
+export default Home;
