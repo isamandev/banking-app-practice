95980622317bdc97492912a7d9d7f2ef53b0b835
Saman Abbasi <samanabbasi.ir@gmail.com>
2024-09-12 13:56:03 +0330

    add right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 function Home() {
-	const LoggedIn = { firstName: 'Adrian' };
+	const LoggedIn = { firstName: 'Adrian', lastName: 'JSM', email: 'gamil@gmail.com' };
 	return (
 		<section className='home'>
 			<div className='home-content'>
@@ -15,7 +16,13 @@ function Home() {
 					/>
 					<TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
 				</header>
+				RECENT TRANSACTIONS
 			</div>
+			<RightSidebar
+				user={LoggedIn}
+				transactions={[]}
+				banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+			/>
 		</section>
 	);
 }
